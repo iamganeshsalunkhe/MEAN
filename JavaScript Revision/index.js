821249182529eb74cb323d77be411f5ee5f1b2f3
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-05-07 17:09:42 +0530

    Basic JS added.

--- JavaScript Revision/index.js
+++ JavaScript Revision/index.js
@@ -8,9 +8,49 @@ console.log(`Hello World`);
 let a = 100
 var b = 200
 console.log(a);
+console.log(typeof(a)); // Number(datatype)
+
 console.log(b);
+console.log(typeof(b)); // Number(datatype);
 
 // constants 
 const FirstName = 'Ganesh'
 
 console.log(FirstName);
+console.log(typeof(FirstName));  //String(datatype);
+
+
+const canVote = true
+console.log(canVote);
+console.log(typeof(canVote)); // Boolean(datatype)
+
+const cand1 = {
+    name:'Ganesh',
+    age:24,
+    city:'Sangli',
+    state:'Maharashtra',
+    country:'India'
+}
+console.log(cand1);
+console.log(typeof(cand1)); // Object (datatype)
+
+
+// functions
+
+// function without parameter
+
+function  Greet(){
+    console.log(`Hello!!!`);
+}
+
+Greet()
+
+
+// function with parameter
+
+function Add(a,b){
+    console.log(a+b);
+}
+
+Add(10,20)
+
