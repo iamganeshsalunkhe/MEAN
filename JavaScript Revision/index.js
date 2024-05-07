813d3074656cca3b241b2cd36b303b4e24888532
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-05-07 16:52:36 +0530

    JavaScript

---
+++ JavaScript Revision/index.js
@@ -0,0 +1,16 @@
+
+// Basic Console Log
+
+console.log(`Hello World`);
+
+// Variable
+
+let a = 100
+var b = 200
+console.log(a);
+console.log(b);
+
+// constants 
+const FirstName = 'Ganesh'
+
+console.log(FirstName);
