fa9a4e3617daada88582f1d2ee0a27cc4e606962
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-05-22 13:04:17 +0530

    Learning TypeScript.

---
+++ TypeScript/page1.ts
@@ -0,0 +1,22 @@
+console.log(`Hello`);
+
+let num:number = 10
+
+console.log(`num : ${num}, typeofnum:${typeof(num)}`);
+
+let myname:string = 'Ganesh'
+
+console.log(`myname : ${myname}, typeofnum:${typeof(myname)}`);
+
+
+let canVote:boolean= true
+
+console.log(`Canvote : ${canVote}, typeOfCanVote:${canVote}`);
+
+// command for running code
+// tsc index.ts; node index.js
+
+
+let num1 :any= 2000
+
+console.log(num1);
---
+++ TypeScript/page2.ts
@@ -0,0 +1,15 @@
+function add(a:number,b:number):number{
+    return a+b
+}
+
+console.log(add(100,2000))
+
+
+function concatation(a:string, b:string):string{
+    return a + b
+}
+
+console.log(concatation('10','20'));
+
+
+
